9212d66f884470d084ff3c8e4da8caa25c4be17a
Shikha Sharma <shikha105789@gmail.com>
2024-12-28 23:54:34 -0800

    login component html and ts code added

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
 
+  loginForm = new FormGroup({ email: this.email, password: this.password });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+  onReset() {
+    this.loginForm.reset();
+  }
 }
