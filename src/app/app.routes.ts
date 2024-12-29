73ae1722f1db2a6421eeb682a7347e3183ede157
Shikha Sharma <shikha105789@gmail.com>
2024-12-28 19:35:06 -0800

    paths added in routes array and auth components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CreateMeetingComponent } from './components/meeting/create-meeting/create-meeting.component';
+import { AllMeetingsComponent } from './components/meeting/all-meetings/all-meetings.component';
+import { RegistrationComponent } from './components/auth/registration/registration.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { ViewMeetingComponent } from './components/meeting/view-meeting/view-meeting.component';
+import { UpdateMeetingComponent } from './components/meeting/update-meeting/update-meeting.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'create-meeting', component: CreateMeetingComponent },
+  { path: 'all-meetings', component: AllMeetingsComponent },
+  { path: 'update-meeting', component: UpdateMeetingComponent },
+  { path: 'view-meeting', component: ViewMeetingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+];
