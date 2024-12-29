cd878597cc94a5d695faa759d1393e347b30534f
Shikha Sharma <shikha105789@gmail.com>
2024-12-28 20:00:45 -0800

    nav bar is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ViewMeetingComponent } from './components/meeting/view-meeting/view-meeting.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ViewMeetingComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
