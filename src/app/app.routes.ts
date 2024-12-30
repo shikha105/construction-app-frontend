d056005b76223bbab60b15c396556d01c46eaa69
Shikha Sharma <shikha105789@gmail.com>
2024-12-29 21:15:09 -0800

    not found component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegistrationComponent } from './components/auth/registration/registrati
 import { LoginComponent } from './components/auth/login/login.component';
 import { ViewMeetingComponent } from './components/meeting/view-meeting/view-meeting.component';
 import { UpdateMeetingComponent } from './components/meeting/update-meeting/update-meeting.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: 'create-meeting', component: CreateMeetingComponent },
@@ -14,4 +15,5 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
   { path: '', redirectTo: '/all-meetings', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
