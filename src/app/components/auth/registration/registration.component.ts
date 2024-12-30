8fa59ab42aa4b1a31f89f5b07670b1d24bec361a
Shikha Sharma <shikha105789@gmail.com>
2024-12-29 20:26:59 -0800

    registration form is added

--- src/app/components/auth/registration/registration.component.ts
+++ src/app/components/auth/registration/registration.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrl: './registration.component.css',
 })
 export class RegistrationComponent {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+  about = new FormControl('');
+  role = new FormControl('apprentice', Validators.required);
 
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    password: this.password,
+    about: this.about,
+    role: this.role,
+  });
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+  onReset() {
+    this.registerForm.reset();
+  }
 }
