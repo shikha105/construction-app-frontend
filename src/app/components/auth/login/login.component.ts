381a2d33f8677d3a6e8a36f989b3779d853f99e3
Shikha Sharma <shikha105789@gmail.com>
2024-12-31 12:53:38 -0800

    login method is implemented

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { response } from 'express';
 
 @Component({
   selector: 'app-login',
@@ -11,19 +13,29 @@ import { Router } from '@angular/router';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
-  email = new FormControl('', [Validators.required, Validators.email]);
+  username = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [
     Validators.required,
     Validators.minLength(6),
   ]);
 
-  loginForm = new FormGroup({ email: this.email, password: this.password });
+  loginForm = new FormGroup({
+    username: this.username,
+    password: this.password,
+  });
 
   onSubmit() {
-    console.log(this.loginForm.value);
-    this.router.navigate(['/']);
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login success:', response);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Error in Login', error.error.message);
+      },
+    });
   }
   onReset() {
     this.loginForm.reset();
