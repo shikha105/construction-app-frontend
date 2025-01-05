adf7aa14c3917431eac919b1c199af01c8abb699
Shikha Sharma <shikha105789@gmail.com>
2025-01-05 15:54:43 -0800

    get all users service implemented

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,13 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { User } from '../../interfaces/user';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
 export class UsersComponent {
   userService = inject(UserService);
+
+  users = this.userService.getAllUsers();
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
