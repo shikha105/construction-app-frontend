e40f4fca5a562cf6368891fc160ac5f5e34a98d5
Shikha Sharma <shikha105789@gmail.com>
2025-01-07 18:55:55 -0800

    validation in pipe

--- src/app/pipes/filter-by-status.pipe.ts
+++ src/app/pipes/filter-by-status.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterByStatusPipe implements PipeTransform {
   transform(meetings: any[], status: string): any[] {
+    if (!meetings) {
+      return [];
+    }
     return meetings.filter((meeting) => meeting.status === status);
   }
 }
