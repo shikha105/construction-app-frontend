1176b1f8783c577e8e5490e6e17b6741f8173d94
Shikha Sharma <shikha105789@gmail.com>
2025-01-14 23:27:33 -0800

    all portfolios component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,7 @@ import { UsersComponent } from './pages/users/users.component';
 import { roleGuard } from './guards/role.guard';
 import { CreatePortfolioComponent } from './components/portfolio/create-portfolio/create-portfolio.component';
 import { ViewPortfolioComponent } from './components/portfolio/view-portfolio/view-portfolio.component';
+import { AllPortfoliosComponent } from './components/portfolio/all-portfolios/all-portfolios.component';
 
 export const routes: Routes = [
   { path: 'meetings/create', component: CreateMeetingComponent },
@@ -40,6 +41,7 @@ export const routes: Routes = [
     data: { roles: ['APPRENTICE'] },
   },
   { path: 'portfolios/view/:id', component: ViewPortfolioComponent },
+  { path: 'portfolios', component: AllPortfoliosComponent },
   {
     path: 'about',
     loadComponent: () =>
